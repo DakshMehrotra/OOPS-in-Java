eb3ba4a8b35ff84c6d760d4b02880a44484d2de5
Daksh Mehrotra <148863185+DakshMehrotra@users.noreply.github.com>
2025-02-12 17:42:20 +0530

    Create 4th.java

---
+++ 4th.java
@@ -0,0 +1,19 @@
+//This is the final experiment of java 4 thfinal 12 th feb
+//package p file a
+package experiment6;  // ✅ Same package name
+
+public class RunAccount {
+    public static void main(String[] args) {
+        Account acc = new Account(5000.75);  // ✅ Creating Account object
+        acc.displayBalance();
+    }
+}
+//package q file b
+package experiment6;  // ✅ Same package name
+
+public class RunAccount {
+    public static void main(String[] args) {
+        Account acc = new Account(5000.75);  // ✅ Creating Account object
+        acc.displayBalance();
+    }
+}
