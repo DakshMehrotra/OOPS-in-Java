60ba5fdd0bad2f480e2f5e9f55de3a89a075695c
Daksh Mehrotra <148863185+DakshMehrotra@users.noreply.github.com>
2025-02-19 19:56:05 +0530

    Create GradeSheet.java

---
+++ GradeSheet.java
@@ -0,0 +1,17 @@
+public class GradeSheet {
+    public static void main(String[] args) {
+        System.out.println("****************************************");
+        System.out.println("*		 GRADE SHEET 		   *");
+        System.out.println("****************************************");
+        System.out.println("Subject		Marks	Grade");
+        System.out.println("----------------------------------------");
+        System.out.println("Math		95	A+");
+        System.out.println("Science		88	A");
+        System.out.println("English		76	B+");
+        System.out.println("History		82	A-");
+        System.out.println("Computer	91	A+");
+        System.out.println("----------------------------------------");
+        System.out.println("*	Overall Percentage: 86.4%	   *");
+        System.out.println("****************************************");
+    }
+}
